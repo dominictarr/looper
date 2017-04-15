68c94b2323328e164fe20a277e686d7738e85b2a
Dominic Tarr <dominic.tarr@gmail.com>
2017-04-16 05:45:19 +1200

    simpler api

--- test/test.js
+++ test/test.js
@@ -4,34 +4,39 @@ var looper = require('../')
 
 tape('n=1000000, with no RangeError', function (t) {
   var n = 1000000, c = 0
-  looper(function (next) {
+  var next = looper(function () {
     c ++
     if(--n) return next()
     t.equal(c, 1000000)
     t.end()
   })
+
+  next()
 })
 
 tape('async is okay', function (t) {
 
   var n = 100, c = 0
-  looper(function (next) {
+  var next = looper(function () {
+    console.log('n, c', n, c)
     c ++
     if(--n) return setTimeout(next)
     t.equal(c, 100)
     t.end()
   })
 
+  next() //start looping
 })
 
 tape('sometimes async is okay', function (t) {
   var i = 1000; c = 0
-  looper(function (next) {
+  var next = looper(function () {
     c++
     if(--i) return Math.random() < 0.1 ? setTimeout(next) : next()
     t.equal(c, 1000)
     t.end()
   })
-
+  next()
 })
+
 
